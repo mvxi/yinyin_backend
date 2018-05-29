198c59ee83a3620fb95d14dbe06cdcd94bf0ca49
mvxi <mvxi@163.com>
2018-05-29 20:32:26 +0800

    add catagory

--- controllers/IndexController.php
+++ controllers/IndexController.php
@@ -16,16 +16,22 @@ class IndexController extends Controller {
     public function actionContent() {
 		$ret = array();
 		$ret['banners'] = $this->bannersInfo();
+		$ret['catagory'] = $this->catagoryInfo();
 		echo Utils::output($ret);
     }
-
     /**
      * get bannerinfo from db.
-     *
      * @return Response|string
      */
     private function bannersInfo() {
 		$productInfos = ProductInfo::find()->where(array('in', 'product_id',  AppConst::$indexBanners))->asArray()->all();
 		return $productInfos;
+    }
+    /**
+     * get catagorynfo. we can set it manually because of not too many
+     * @return Response|string
+     */
+    private function catagoryInfo() {
+		return AppConst::$indexCatagory;
     }
 }
--- widgets/AppConst.php
+++ widgets/AppConst.php
@@ -7,6 +7,15 @@ class AppConst {
 	public static $mallName = array(
 		'mallName' => '本真生活',
 	);
+	public static $indexCatagory = array(
+		'面部护理',
+		'身体护理',
+		'休闲食品',
+		'保健养生',
+		'休闲娱乐',
+	);
+
+	// internal code
 	public static $indexBanners = array(
 		'105b0019c0dcceb',
 		'105b003b89a5e19',
