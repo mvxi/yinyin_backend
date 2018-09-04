1b9866961bd5684b55b6c58195b138f3b32a0c89
mvxi <mvxi@163.com>
2018-09-04 20:19:15 +0800

    add bj db

--- config/db.php
+++ config/db.php
@@ -2,8 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=211.159.215.250:63277;dbname=yinyin',
+    #'dsn' => 'mysql:host=211.159.215.250:63277;dbname=yinyin',
     #'dsn' => 'mysql:host=sh-cdb-a8dtw16v.sql.tencentcdb.com;dbname=test',
+    'dsn' => 'mysql:host=140.143.217.15:10022;dbname=yinyin',
     'username' => 'shop',
     'password' => 'dyh80882',
     'charset' => 'utf8',
